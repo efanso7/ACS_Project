5928637e6b79c57f333d66b0f244d777a8064683
Ethan Blow <02ethan.blow@gulllakecs.org>
2019-12-03 09:40:29 -0500

    add cards and pick card function note: i meant to branch this but apparently I didnt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,22 @@ package com.company;
 
 public class Main {
 
+    public static double pickcard(){
+        double x = (Math.random() * ((13 - 1)+1))+1;
+        return x;
+    }
+
+    public static void cards() {
+        int card1 = (int)pickcard();
+        int card2 = (int)pickcard();
+        int card3 = (int)pickcard();
+        int card4 = (int)pickcard();
+        int card5 = (int)pickcard();
+
+        System.out.println("Your cards: " + card1 + ", " + card2 + ", " + card3 + ", " + card4 + ", " + card5);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        cards();
     }
 }
