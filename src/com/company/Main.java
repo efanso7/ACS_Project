1cc3ee1080b9af289da90f61b2c79f46df1c9f3a
Ethan Blow <02ethan.blow@gulllakecs.org>
2019-12-10 09:57:29 -0500

    2 rounds note: next class try using a while loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -201,5 +201,144 @@ public class Main {
 
 
 
+        //round 2
+
+        //dont print cards already played
+
+        //player1
+        if (card1 == 0) {
+            System.out.println("Player 1: " + card3 + ", " + card5 + ", " + card7 + ", " + card9);
+        }
+        else if (card3 == 0) {
+            System.out.println("Player 1: " + card1 + ", " + card5 + ", " + card7 + ", " + card9);
+        }
+        else if (card5 == 0) {
+            System.out.println("Player 1: " + card1 + ", " + card3 + ", " + card7 + ", " + card9);
+        }
+        else if (card7 == 0) {
+            System.out.println("Player 1: " + card1 + ", " + card3 + ", " + card5 + ", " + card9);
+        }
+        else if (card9 == 0) {
+            System.out.println("Player 1: " + card1 + ", " + card3 + ", " + card5 + ", " + card7);
+        }
+        else{
+            System.out.println("Player 1: " + card1 + ", " + card3 + ", " + card5 + ", " + card7 + ", " + card9);
+        }
+
+        //player2
+        if (card2 == 0) {
+            System.out.println("Player 2: " + card4 + ", " + card6 + ", " + card8 + ", " + card10);
+        }
+        else if (card4 == 0) {
+            System.out.println("Player 2: " + card2 + ", " + card6 + ", " + card8 + ", " + card10);
+        }
+        else if (card6 == 0) {
+            System.out.println("Player 2: " + card2 + ", " + card4 + ", " + card8 + ", " + card10);
+        }
+        else if (card8 == 0) {
+            System.out.println("Player 2: " + card2 + ", " + card4 + ", " + card6 + ", " + card10);
+        }
+        else if (card10 == 0) {
+            System.out.println("Player 2: " + card2 + ", " + card4 + ", " + card6 + ", " + card8);
+        }
+        else{
+            System.out.println("Player 2: " + card2 + ", " + card4 + ", " + card6 + ", " + card8 + ", " + card10);
+        }
+
+        System.out.println("");
+
+        //ask which card each player wants to play
+
+        //player 1
+        System.out.println("player 1 what card do you want to play: ");
+        cardPlayed = sc.nextInt();
+        if (cardPlayed > 5 || cardPlayed < 1) {
+            while (cardPlayed > 5 || cardPlayed < 1) {
+                System.out.println("player 1 what card do you want to play (it must be between 5 and 1): ");
+                cardPlayed = sc.nextInt();
+            }
+        }
+        else{
+            if (cardPlayed == 1){
+                player1card = card1;
+                card1 = 0;
+            }
+            else if (cardPlayed == 2){
+                player1card = card3;
+                card3 = 0;
+            }
+            else if (cardPlayed == 3){
+                player1card = card5;
+                card5 = 0;
+            }
+            else if (cardPlayed == 4){
+                player1card = card7;
+                card7 = 0;
+            }
+            else if (cardPlayed == 5){
+                player1card = card9;
+                card9 = 0;
+            }
+            System.out.println("player 1 played a " + player1card);
+            System.out.println("");
+
+        }
+
+        //player 2
+        System.out.println("player 2 what card do you want to play: ");
+        cardPlayed = sc.nextInt();
+        if (cardPlayed > 5 || cardPlayed < 1) {
+            while (cardPlayed > 5 || cardPlayed < 1) {
+                System.out.println("player 2 what card do you want to play (it must be between 5 and 1): ");
+                cardPlayed = sc.nextInt();
+            }
+        }
+        else{
+            if (cardPlayed == 1){
+                player2card = card2;
+                card2 = 0;
+            }
+            else if (cardPlayed == 2){
+                player2card = card4;
+                card4 = 0;
+            }
+            else if (cardPlayed == 3){
+                player2card = card6;
+                card6 = 0;
+            }
+            else if (cardPlayed == 4){
+                player2card = card8;
+                card8 = 0;
+            }
+            else if (cardPlayed == 5) {
+                player2card = card10;
+                card10 = 0;
+            }
+            System.out.println("player 2 played a " + player2card);
+            System.out.println("");
+        }
+
+        //decide who wins
+
+        //player 1 wins
+        if (player1card > player2card){
+            System.out.println("player 1 wins");
+            System.out.println("");
+            player1points = player1points + 1;
+        }
+
+        //player 2 wins
+        else if (player1card < player2card){
+            System.out.println("player 2 wins");
+            System.out.println("");
+            player2points = player2points + 1;
+        }
+
+        // print score
+        System.out.println("score");
+        System.out.println("");
+        System.out.println("player1: " + player1points);
+        System.out.println("player2: " + player2points);
+        System.out.println("");
     }
 }
